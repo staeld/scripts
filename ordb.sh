57352c7bb697b2ff88e6ffd2d6a5e37c2573e30d
Stæld Lakorv <staeld@staeld.co.cc>
2012-10-24 10:55:55 +0200

    A little wrapper for a Norwegian dictionary script

---
+++ ordb.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# ordb.sh - enkel wrappar for å bruka ordbanken som ordbok
+# Opphavsrett Stæld Lakorv, 2012 <staeld@illumine.ch>
+# Sluppe under GPLv3+, sjå <http://www.gnu.org/licenses/>
+
+function help() {
+    echo "Bruk: grunnord [kriterium1] [kriterium2] ..."
+    echo
+    echo "Dette slår opp ordet «grunnord» i fullformsordlista til Norsk ordbank,"
+    echo "med eventuell filtrering etter eitt eller fleire kriterium."
+    echo "Mulige kriterie er m.a. «subst», «fem» (hokjønn), «verb», «pret» (preteritum),"
+    echo "og så vidare." # Grunnordet kan også innehalda regulære uttrykk."
+    echo "Skriptet gjer bruk av «ordbanken» av Karl Ove Hufthammer <karl@huftis.org>."
+}
+
+if [ -z $1 ]; then
+    until [ "$spr" == "nn" ] || [ "$spr" == "nb" ]; do
+        read -p "Språk? [nn/nb]: " spr
+        if [ -z "$spr" ]; then
+            spr="nn"
+        fi
+    done
+else
+    case $1 in
+        *help|*hjelp|-h)
+            help
+            exit 0
+            ;;
+        nn|nyn*)
+            spr="nn"
+            ;;
+        nb|bok*)
+            spr="nb"
+            ;;
+    esac
+fi
+
+while [ 1 ]; do
+    clear
+    read -e -p "Ord: " ord
+    if [ "$ord" == "quit" ] || [ "$ord" == "exit" ] || [ "$ord" == "avslutt" ]; then
+        exit 0
+    elif [ -z "$ord" ]; then
+        help
+    else
+        echo "----"
+        # Regexp kutta ut fordi alle ord er delvise treff og ikkje heile ord
+        ordbanken -s $spr -- $ord
+    fi
+    echo "----"
+    read -p "Trykk enter for nytt søk..."
+done
